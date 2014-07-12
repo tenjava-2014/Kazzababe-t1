b2efdd76be715a445805e1cbd72b3f3fe95e8df5
Mckelle Gravelle <GravelleMckelle@gmail.com>
2014-07-11 19:23:28 -0500

    Test

--- src/main/java/com/tenjava/entries/Kazzababe/t1/TenJava.java
+++ src/main/java/com/tenjava/entries/Kazzababe/t1/TenJava.java
@@ -2,4 +2,15 @@ package com.tenjava.entries.Kazzababe.t1;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
